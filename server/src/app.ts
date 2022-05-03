7e00aceb1880e3d421fcc2a9e01dc5aec8cb5f25
Hongbusi <q951686662@qq.com>
2022-05-03 16:19:41 +0800

    chore: init server

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import Koa from 'koa'
+
+const app = new Koa()
+
+app.use(async(ctx) => {
+  ctx.body = 'Hello World'
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
