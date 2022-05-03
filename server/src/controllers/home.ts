7df2344b69e392ac6bb72d569344ef6305294fce
Hongbusi <q951686662@qq.com>
2022-05-03 21:57:01 +0800

    chore: optimization

--- server/src/controllers/home.ts
+++ server/src/controllers/home.ts
@@ -1,9 +1,8 @@
-// import path from 'path'
+import path from 'path'
 import fse from 'fs-extra'
 import type { Context } from 'koa'
-// import { staticDir } from '../configs'
-
-import { CONTROLLER, GET } from '../decorator'
+import { staticDir } from '../configs'
+import { CONTROLLER, POST } from '../decorator'
 
 @CONTROLLER()
 class HomeController {
@@ -12,22 +11,20 @@ class HomeController {
     return fse.existsSync(dirPath) ? (await fse.readdir(dirPath)).filter(name => name[0] !== '.') : []
   }
 
-  @GET('/check')
+  @POST('/check')
   async check(ctx: Context) {
-    // const { ext, hash } = ctx.request.body
-    // const filePath = path.resolve(staticDir, `${hash}.${ext}`)
+    const { ext, hash } = ctx.request.body
+    const filePath = path.resolve(staticDir, `${hash}.${ext}`)
 
-    const uploaded = false
-    const uploadedList: any[] = []
-    console.log('check~~~')
-    console.log(this)
-    // if (fse.existsSync(filePath)) {
-    //   uploaded = true
-    // }
-    // else {
-    //   // 文件没有完全上传完毕，但是可能存在的部分切片上传完毕了
-    //   uploadedList = await this.getUploadList(path.resolve(staticDir, hash))
-    // }
+    let uploaded = false
+    let uploadedList: any[] = []
+    if (fse.existsSync(filePath)) {
+      uploaded = true
+    }
+    else {
+      // 文件没有完全上传完毕，但是可能存在的部分切片上传完毕了
+      uploadedList = await this.getUploadList(path.resolve(staticDir, hash))
+    }
     ctx.body = {
       uploaded,
       uploadedList
